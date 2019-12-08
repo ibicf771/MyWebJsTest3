65d5c371a550f90c2fb336805457f2e9bb47cd00
yangsimin <181326622@qq.com>
2019-12-08 17:42:10 +0800

    使用jsbridge框架的android js交互

--- app/src/main/java/com/aegis/res/plugin/mywebjstest3/MainActivity.java
+++ app/src/main/java/com/aegis/res/plugin/mywebjstest3/MainActivity.java
@@ -8,56 +8,68 @@ import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.github.lzyzsd.jsbridge.BridgeHandler;
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView mWebView;
+//    private WebView mWebView;
+    private BridgeWebView bridgeWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bridgeWebView = findViewById(R.id.web_view);
 
-        mWebView = findViewById(R.id.web_view);
-        WebSettings webSettings = mWebView.getSettings();
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN)
-        {
-            webSettings.setAllowUniversalAccessFromFileURLs(true);
-        }
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setSupportZoom(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setJavaScriptEnabled(true);
-//        mWebView.loadUrl("https://maps.epgis.com/");
-
-        mWebView.addJavascriptInterface(new AndroidtoJs(), "gps");
-        mWebView.loadUrl("file:///android_asset/index2.html");
-
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        bridgeWebView.setDefaultHandler(new BridgeHandler() {
             @Override
-            public void onClick(View view) {
-                String writeResult = "javascript:showWriteResult('" + 1231111 + "')";
-                mWebView.loadUrl(writeResult);
+            public void handler(String data, CallBackFunction function) {
+                String msg = "默认接收到js的数据：" + data;
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+
+                function.onCallBack("java默认接收完毕，并回传数据给js"); //回传数据给js
             }
         });
 
+        bridgeWebView.registerHandler("submitFromWeb", new BridgeHandler() {
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                String msg = "指定接收到js的数据：" + data;
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
 
-    }
-
-    private class MyWebViewClient extends WebViewClient {
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            return true;
-        }
-
-
-        public void onPageFinished(WebView view, String url) {
-            CookieManager cookieManager = CookieManager.getInstance();
-            String CookieStr = cookieManager.getCookie(url);
-            if (CookieStr != null) {
-                Log.i("cookie", CookieStr);
+                function.onCallBack("java指定接收完毕，并回传数据给js"); //回传数据给js
             }
-            super.onPageFinished(view, url);
-        }
+        });
 
+        bridgeWebView.loadUrl("file:///android_asset/index2.html");
 
+        findViewById(R.id.JavaToJsDefault).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bridgeWebView.send("发送数据给js默认接收", new CallBackFunction() {
+                    @Override
+                    public void onCallBack(String data) { //处理js回传的数据
+                        Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.JavaToJsSpec).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bridgeWebView.callHandler("functionInJs", "发送数据给js指定接收", new CallBackFunction() {
+                    @Override
+                    public void onCallBack(String data) { //处理js回传的数据
+                        Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
+
 }
